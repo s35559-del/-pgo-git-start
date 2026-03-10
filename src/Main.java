bfa8a7703a57f13703a587be57d5dfb058ef64d9
Pękalski Piotr <s35559@pjwstk.edu.pl>
2026-03-10 09:35:00 +0100

    Add final workflow completion message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("Application info: simple demo project for Git workflow.");
+        System.out.println("Repository workflow finished.");
     }
 }
