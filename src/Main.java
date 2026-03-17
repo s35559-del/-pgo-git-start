d07f2442edea2eecf385f1668e2c58f1d505408d
Pękalski Piotr <s35559@pjwstk.edu.pl>
2026-03-17 14:08:59 +0100

    Add helper information about the application

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
+        System.out.println("Hello, Git! Feature info branch update.");
         System.out.println("Application info: simple demo project for Git workflow.");
         System.out.println("Repository workflow finished.");
     }
